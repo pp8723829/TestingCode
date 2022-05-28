78af0f70ce3069d3f31fef0662237fa0f134dee4
RLIN <p8723829@gmail.com>
2022-05-28 10:50:19 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import discord
-from discord.exe import commands
+from discord.ext import commands
 
-bot=commands.bot(command_prefix= '@')
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print('>>機器人狀態:線上<<')
+    print('Done!!')
 
 bot.run('OTc5OTI2NzUyNTE0MTU4NjAz.GK8VfV.nMqcA4K595RcLDHm9lPuZ_7xcBZNzJH7Z-3u1Q')
