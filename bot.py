9307133f980e6c7264b058b114e3053a3e784651
RLIN <p8723829@gmail.com>
2022-05-28 11:43:50 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,17 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'延遲: {round(bot.latency*1000)} 毫秒')
 
 @bot.event
 async def on_ready():
-    print('Done!!')
+    channel=bot.get_channel(859571060701790211)
+    await channel.send('Status:**``Online``**)'
+    f'延遲 {round(bot.latency*1000)} 毫秒')
+    print('Done')
+
+
 
 bot.run('OTc5OTI2NzUyNTE0MTU4NjAz.GK8VfV.nMqcA4K595RcLDHm9lPuZ_7xcBZNzJH7Z-3u1Q')
